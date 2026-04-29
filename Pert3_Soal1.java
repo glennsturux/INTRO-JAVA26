2b291196ed69733e8a0434c67c1480ce278483db
glennsturux <glenleinz@gmail.com>
2026-04-29 21:11:00 +0700

    Create Pert3_Soal1.java

---
+++ Pert3_Soal1.java
@@ -0,0 +1,10 @@
+public class Soal1 {
+    public static void main(String[] args) {
+        int angka1 = Integer.parseInt(args[0]);
+        int angka2 = Integer.parseInt(args[1]);
+
+        int hasil = angka1 / angka2;
+        System.out.println("Hasil Pembagian: " + hasil);
+    }
+}
+
